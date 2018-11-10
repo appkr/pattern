a8732c829b534cab266e2c71c47c5e29e60e8986
appkr <juwonkim@me.com>
2018-11-10 16:17:12 +0900

    make isPrime(4) pass

--- Kata/src/PrimeNumber.php
+++ Kata/src/PrimeNumber.php
@@ -6,6 +6,16 @@ class PrimeNumber
 {
     public function isPrime($n)
     {
+        if ($n == 2) {
+            return true;
+        }
+        if ($n == 3) {
+            return true;
+        }
+        if ($n % 2 == 0) {
+            return false;
+        }
+
         return true;
     }
 }
