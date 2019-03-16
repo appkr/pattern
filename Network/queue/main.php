8fe75558f84c4249e0744c0e5c0c52592434ed53
appkr <juwonkim@me.com>
2019-03-16 23:57:44 +0900

    큐 이해하기 #2: 이벤트핸들러에서 큐를 이용해 비동기 처리하기

--- Network/queue/main.php
+++ Network/queue/main.php
@@ -1,13 +1,27 @@
 <?php
 
 require __DIR__.'/./Queue.php';
+require __DIR__.'/../../Behavioral/Observer3/Event.php';
+require __DIR__.'/../../Behavioral/Observer3/Handler.php';
+require __DIR__.'/../../Behavioral/Observer3/EventDispatcher.php';
 
 $queue = new Queue();
 
-$queue->push('I am message 1');
-sleep(1);
+class ADomainEvent implements Event {
+    public $data = 'I am a domain event. Somebody handle me!';
+}
 
-$queue->push('I am message 2');
-sleep(2);
+class AEventHandler implements Handler {
+    private $queue;
+    public function __construct(Queue $queue) {
+        $this->queue = $queue;
+    }
+    public function handle(Event $event) {
+        $this->queue->push($event->data);
+    }
+}
 
-$queue->push('I am message 3');
+$event = new ADomainEvent();
+$handler = new AEventHandler($queue);
+EventDispatcher::registerPair(ADomainEvent::class, $handler);
+EventDispatcher::fire($event);
